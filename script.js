da3a11e2130c2bda17d9976cdcfdac83c0fe67bf
rabidfae <lauraelizabeth.watkin@gmail.com>
2024-08-28 12:59:17 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//At least 2 functions
+// create todo variables that will best represent my todo
+//Data model should store all info about todos
+//todo name
+//status
+//id
+//category
+//due date
+//Functions should allow access to the data model for the following procedures:
+//Edit Todos
+//Edit Status
+//Edit Category
+//Edit Due Date
+//Complete Todo
+//Delete Todo
+//Delete Category
+//Add New Todo
+//Add New Category
