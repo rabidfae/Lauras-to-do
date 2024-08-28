ab6e59630b261cb821ff889f0daad208a25d7519
rabidfae <lauraelizabeth.watkin@gmail.com>
2024-08-28 14:01:18 -0600

    added category. need to figure js for delete/edit

--- script.js
+++ script.js
@@ -21,22 +21,41 @@
 
 const newInput = document.getElementById('newInput'); //get info from input
 const newDate = document.getElementById('newDate');//get info on due date
+const newCat = document.getElementById('newCat');//get info on category
 const taskList = document.getElementById('taskList');//list of tasks added by input
 
 function addTask () { //function to add task
-    const taskText = newInput.value.trim();
+    const taskText = newInput.value.trim();// new todo
     if (taskText !==''){
-        const li = document.createElement ('li'); //  create list item from task t
+        const li = document.createElement ('li'); //  create list item from task 
+        li.addEventListener('click', completeTask) //complete task should remove it
         li.textContent = taskText;
         taskList.appendChild(li);
         newInput.value = ''; //add input
 
     }
-    const taskText2 = newDate.value.trim();
+    const taskText2 = newDate.value.trim(); //new due date
     if (taskText !==''){
         const li = document.createElement ('li'); //  create list item from task 
+        li.addEventListener('click', completeTask) //complete task should remove it
         li.textContent = taskText2;
         taskList.appendChild(li);
         newDate.value = ''; //add due date
+      
     }
+    const taskText3 = newCat.value.trim(); //new category
+    if (taskText !==''){
+        const li = document.createElement ('li'); //  create list item from task 
+        li.addEventListener('click', completeTask) //complete task should remove it
+        li.textContent = taskText3;
+        taskList.appendChild(li);
+        newCat.value = ''; //add due date
+      
+    }
+}
+
+
+function completeTask(event) {
+    const task = event.target;
+    task.classList.toggle('completed')
 }
